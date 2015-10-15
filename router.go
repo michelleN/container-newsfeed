3371a9f4b671914674ea4e2795b3ff1d6bd2aca9
Michelle Noorali <michellemolu@gmail.com>
2015-10-15 09:20:30 -0700

    return urls to issues

--- router.go
+++ router.go
@@ -1,27 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html"
 	"io/ioutil"
-	//"log"
-	"encoding/json"
+	"log"
 	"net/http"
 	"os"
 
-	//"github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 )
 
 func main() {
 
-	//router := mux.NewRouter().StrictSlash(true)
-	//router.HandleFunc("/", Index)
-	fmt.Println(github())
-	//log.Fatal(http.ListenAndServe(":8080", router))
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	issues := github("https://api.github.com/repos/deis/deis/issues")
+	fmt.Fprintf(w, issues, html.EscapeString(r.URL.Path))
 	os.Exit(0)
 }
 
@@ -31,31 +31,26 @@ func check(e error) {
 	}
 }
 
-func github() string {
+func github(repo string) string {
 	client := &http.Client{}
 	oauthToken := os.Getenv("OAUTH_TOKEN")
-	req, _ := http.NewRequest("GET", "https://api.github.com/repos/deis/deis/issues", nil)
+	req, _ := http.NewRequest("GET", repo, nil)
 	req.Header.Add("Authorization", fmt.Sprintf("token %s", oauthToken))
 	r, e := client.Do(req)
 	check(e)
-	fmt.Println("1")
 	defer r.Body.Close()
 
-	var data GithubResponse
+	var issues []*GithubIssue
 	body, e := ioutil.ReadAll(r.Body)
-	fmt.Println("2")
 	check(e)
-	err := json.Unmarshal(body, &data)
-	fmt.Println("3")
+	err := json.Unmarshal(body, &issues)
 	check(err)
-	return data.issues[0].url
-}
-
-type GithubResponse struct {
-	issues map[string]GithubIssue
+	fmt.Println(issues[0].Title)
+	return issues[0].Url
 }
 
 type GithubIssue struct {
-	url string `json:"html_url"`
+	Url   string `json:"html_url"`
+	Title string `json:"title"`
 	//github_id int    `json:"id"`
 }
